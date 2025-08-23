04dfa71129db91d492ce53c3eddea724d630f71a
Fab <51055703+CreepsoOff@users.noreply.github.com>
2025-08-23 14:21:21 +0200

    feat: add jump button for overlays

--- src/core/util.ts
+++ src/core/util.ts
@@ -1,3 +1,5 @@
+import { TILE_SIZE } from './constants';
+
 export function uid() {
   return `${Date.now().toString(36)}-${Math.random().toString(36).slice(2,10)}`;
 }
@@ -8,4 +10,30 @@ export function uniqueName(base: string, existing: string[]) {
   let i = 1;
   while (names.has(`${base} (${i})`.toLowerCase())) i++;
   return `${base} (${i})`;
+}
+
+const a = 2 * Math.PI * 6378137 / 2;
+const b = (a / TILE_SIZE) / 2 ** 10;
+
+export function pixelToLonLat(x: number, y: number): [number, number] {
+  const lon = (x * b - a) / a * 180;
+  let lat = (a - y * b) / a * 180;
+  lat = 180 / Math.PI * (2 * Math.atan(Math.exp(lat * Math.PI / 180)) - Math.PI / 2);
+  return [lon, lat];
+}
+
+export function lonLatToPixel(lon: number, lat: number) {
+  lat = Math.log(Math.tan((90 + lat) * Math.PI / 360)) / (Math.PI / 180);
+  const x = (lon / 180 * a + a) / b;
+  const y = (a - lat / 180 * a) / b;
+  return [Math.floor(x), Math.floor(y)];
+}
+
+export function selectPixel(x: number, y: number, zoom: number) {
+  const lonLat = pixelToLonLat(x + 0.5, y + 0.5);
+  unsafeWindow.localStorage.setItem('location', JSON.stringify({
+    lng: lonLat[0],
+    lat: lonLat[1],
+    zoom
+  }));
 }
